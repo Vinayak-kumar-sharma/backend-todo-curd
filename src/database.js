24f84da36b55a3f7a6b8dc0382a378268092be6a
Vinayak-kumar-sharma <vinayaksharma07@yahoo.com>
2024-07-15 11:20:11 +0530

    fix

--- src/database.js
+++ src/database.js
@@ -2,9 +2,9 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password:"1319",
+  password:"password",
   port:5432,
-  database:"todo"
+  database:"DB_NAME"
 });
 
 
